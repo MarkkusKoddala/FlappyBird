99893af0be2c632068bad457e32a2cd0ec35648c
markkusk <koddalamarkkus@gmail.com>
2023-03-16 13:22:41 +0200

    uuendus

--- src/FlappyBird/Paneel.java
+++ src/FlappyBird/Paneel.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.Random;
 
 //https://www.youtube.com/watch?v=a3Hzs2XAJBg
 public class Paneel extends JPanel implements KeyListener, ActionListener {
@@ -19,9 +18,8 @@ public class Paneel extends JPanel implements KeyListener, ActionListener {
     private ArrayList<Takistus> takistusteList;
 
 
-
-    public Paneel(){
-        mängijaRuut= new Mängija(400);
+    public Paneel() {
+        mängijaRuut = new Mängija(400);
         takistusteList = new ArrayList<Takistus>();
         setPreferredSize(new Dimension(LAIUS, KÕRGUS));
         setBackground(new Color(31, 158, 208));
@@ -33,19 +31,19 @@ public class Paneel extends JPanel implements KeyListener, ActionListener {
     }
 
     @Override
-    public void paintComponent (Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(new Color(38, 129, 216));
-        g.fillRect(0,0,LAIUS,KÕRGUS);
+        g.fillRect(0, 0, LAIUS, KÕRGUS);
 
         g.setColor(new Color(10, 189, 15));
-        g.fillRect(0, KÕRGUS-100, LAIUS, 20);
+        g.fillRect(0, KÕRGUS - 100, LAIUS, 20);
 
         g.setColor(new Color(192, 106, 4));
-        g.fillRect(0, KÕRGUS-80, LAIUS, 80 );
+        g.fillRect(0, KÕRGUS - 80, LAIUS, 80);
 
-        if (!(mängKäib)){
-            Font font = new Font("Arial", Font.BOLD,  25);
+        if (!(mängKäib)) {
+            Font font = new Font("Arial", Font.BOLD, 25);
             g.setFont(font);
             g.drawString("Vajuta ENTER, et alustada mängu!", 120, 300);
 
@@ -57,81 +55,82 @@ public class Paneel extends JPanel implements KeyListener, ActionListener {
         }
     }
 
-    public void keyPressed (KeyEvent e) {
-        if (!mängKäib && e.getKeyCode() == KeyEvent.VK_ENTER){
+    public void keyPressed(KeyEvent e) {
+        if (!mängKäib && e.getKeyCode() == KeyEvent.VK_ENTER) {
             alustaMängu();
 
-        }else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             hüppa();
         }
     }
 
-    public void actionPerformed (ActionEvent a){
+    public void actionPerformed(ActionEvent a) {
         mänguMootor();
-         repaint();
+        repaint();
     }
 
-    public void mänguMootor(){
-        if(mängKäib){
+    public void mänguMootor() {
+        if (mängKäib) { //kui mäng algab, siis kontrollib seda If-i
             kontrolliJagenereeriTakistused();
 
-            if (!(takistusteList.get(0).getX() + 50 > 0)){
+            if (!(takistusteList.get(0).getX() + 50 > 0)) {
                 takistusteList.remove(0);
                 takistusteList.remove(0);
                 takistusteArv -= 2;
             }
 
-            for (Takistus takistus: takistusteList){
+            for (Takistus takistus : takistusteList) {
                 takistus.takistusLiigub();
             }
             mängijaRuut.liigub();
-            kontrolliPuudet();}
-        else setVisible(false);
+            kontrolliPuudet();
+        } else setVisible(false); // siin peaks siis olema juhul, kui mingi kokkupõrge
 
     }
 
-    public void kontrolliJagenereeriTakistused(){
-        while (takistusteArv < 4){
+    public void kontrolliJagenereeriTakistused() {
+        while (takistusteArv < 4) {
             if (takistusteList.size() == 0) {
-                int kõrgus = (int) (Math.random() * 200) +120;
+                int kõrgus = (int) (Math.random() * 200) + 120;
                 takistusteList.add(new Takistus(800, 0, kõrgus));
-                takistusteList.add(new Takistus(800, kõrgus+120, 380-kõrgus ));
+                takistusteList.add(new Takistus(800, kõrgus + 120, 380 - kõrgus));
             } else {
-                int kõrgus = (int) (Math.random() * 200) +120;
-                takistusteList.add(new Takistus(takistusteList.get(takistusteList.size()-2).getX() + TAKISTUSTEVAHE + 50, 0, kõrgus));
-                takistusteList.add(new Takistus(takistusteList.get(takistusteList.size()-2).getX() + TAKISTUSTEVAHE + 50, kõrgus+120, 380-kõrgus));
+                int kõrgus = (int) (Math.random() * 200) + 120;
+                takistusteList.add(new Takistus(takistusteList.get(takistusteList.size() - 2).getX() + TAKISTUSTEVAHE + 50, 0, kõrgus));
+                takistusteList.add(new Takistus(takistusteList.get(takistusteList.size() - 2).getX() + TAKISTUSTEVAHE + 50, kõrgus + 120, 380 - kõrgus));
             }
             takistusteArv += 2;
         }
     }
 
-    public void kontrolliPuudet (){
+    public void kontrolliPuudet() {
         int ruudu1nurkX = mängijaRuut.getX();
         int ruudu1nurkY = mängijaRuut.getY() + mängijaRuut.getRUUDUA();
         int ruudu2nurkX = mängijaRuut.getX() + mängijaRuut.getRUUDUA();
         int ruudu2nurkY = mängijaRuut.getY();
-        if ( ruudu1nurkX <= takistusteList.get(0).getX()+takistusteList.get(0).getKõrgus() && ruudu1nurkY <=takistusteList.get(0).getY()){
+        if (ruudu1nurkX <= takistusteList.get(0).getX() + takistusteList.get(0).getKõrgus() && ruudu1nurkY <= takistusteList.get(0).getY()) {
             mängKäib = false;
         }
-        if (ruudu2nurkY+20 >= 520) mängKäib = false;
+        if (mängijaRuut.getY() + mängijaRuut.getRUUDUA() >= 500)
+            mängKäib = false; // kontrollib hetke, kui paneb paugu vastu maad
 
 
     }
 
     private void hüppa() {
-        mängijaRuut.setyKiirus(-4);
+        mängijaRuut.setyKiirus(-5);
     }
 
-    public void alustaMängu(){
+    public void alustaMängu() {
         mängKäib = true;
         timer.start();
     }
 
-    public void keyReleased (KeyEvent e){ //ei lähe vaja, aga kuna liideses on see olemas, siis...
+    public void keyReleased(KeyEvent e) { //ei lähe vaja, aga kuna liideses on see olemas, siis...
 
     }
 
-    public void keyTyped (KeyEvent e){
+    public void keyTyped(KeyEvent e) {
 
     }
 
